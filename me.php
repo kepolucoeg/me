993cd4034ed14ae038e2a4facedfe0c832e91898
kepolucoeg <41155366+kepolucoeg@users.noreply.github.com>
2018-07-13 22:57:15 +0700

    Update me.php

--- me.php
+++ me.php
@@ -48,7 +48,7 @@ $app->post('/', function ($request, $response)
 		$userMessage = $event['message']['text'];
 		if(strtolower($userMessage) == '01')
 		{
-			$message = "Kingdom";
+			$message = "Kingdomm";
             $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
 			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
 			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
